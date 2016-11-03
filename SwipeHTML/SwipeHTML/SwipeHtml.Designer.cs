9284b5f753a4e77ed48ef9d529fdefda0d362134
grafexum <charlesexum@gmail.com>
2016-11-02 20:12:22 -0700

    Add files via upload

---
+++ SwipeHTML/SwipeHTML/SwipeHtml.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace SwipeHTML
+{
+    partial class SwipeHtml
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInputBox = new System.Windows.Forms.TextBox();
+            this.btnDoScrap = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInputBox
+            // 
+            this.txtInputBox.Location = new System.Drawing.Point(9, 12);
+            this.txtInputBox.Name = "txtInputBox";
+            this.txtInputBox.Size = new System.Drawing.Size(263, 20);
+            this.txtInputBox.TabIndex = 0;
+            // 
+            // btnDoScrap
+            // 
+            this.btnDoScrap.Location = new System.Drawing.Point(9, 38);
+            this.btnDoScrap.Name = "btnDoScrap";
+            this.btnDoScrap.Size = new System.Drawing.Size(263, 88);
+            this.btnDoScrap.TabIndex = 1;
+            this.btnDoScrap.Text = "SCRAP!";
+            this.btnDoScrap.UseVisualStyleBackColor = true;
+            this.btnDoScrap.Click += new System.EventHandler(this.btnDoScrap_Click);
+            // 
+            // SwipeHtml
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 137);
+            this.Controls.Add(this.btnDoScrap);
+            this.Controls.Add(this.txtInputBox);
+            this.Name = "SwipeHtml";
+            this.Text = "Swipe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInputBox;
+        private System.Windows.Forms.Button btnDoScrap;
+    }
+}
+
---
+++ SwipeHTML/SwipeHTML/SwipeHtml.cs
@@ -0,0 +1,63 @@
+﻿//2016.11.02 Graf Exum
+//http://www.github.com/grafexum
+
+
+
+using System;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SwipeHTML
+{
+    public partial class SwipeHtml : Form
+    {
+        public SwipeHtml()
+        {
+            InitializeComponent();
+        }
+
+        public static String ReturnHTML(string Url)
+        {
+            HttpWebRequest myRequest = (HttpWebRequest)WebRequest.Create(Url);
+            myRequest.Method = "GET";
+            WebResponse myResponse = myRequest.GetResponse();
+            StreamReader sr = new StreamReader(myResponse.GetResponseStream(), System.Text.Encoding.UTF8);
+            string result = sr.ReadToEnd();
+            sr.Close();
+            myResponse.Close();
+
+            return result;
+        }
+
+
+        private void btnDoScrap_Click(object sender, EventArgs e)
+        {
+            var InputText = txtInputBox.Text;
+            //ReturnHTML(InputText);
+            SaveFileDialog Save = new SaveFileDialog();
+            Save.Filter = "Html files (*.html)|*.html";
+            try
+            {
+                if (Save.ShowDialog() == DialogResult.OK)
+                {
+                    using (StreamWriter sw = new StreamWriter(Save.FileName))
+                        sw.WriteLine(ReturnHTML(InputText));
+                }
+            }
+            catch
+            {
+                txtInputBox.Text = "Error!!";
+            }
+        }
+    }
+}
